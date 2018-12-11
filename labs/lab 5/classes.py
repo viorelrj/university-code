9b9182ce633ab05c8468bd702d3ba2dc05175cd0
Viorel Rinja <viorelrinja@gmail.com>
2018-12-11 22:29:46 +0200

    implpement functionality of BST node removal, together with appendix methods like find min and max, search in a BST, checking the presence of a child or the own branching direction. Implement the method to get a binary tree by levels

--- labs/lab 5/classes.py
+++ labs/lab 5/classes.py
@@ -5,11 +5,39 @@ class Node:
 		self.left = None
 		self.right = None
 		self.parent = None
+		self.branch = None
+		self.depth = 0
+
+	def __has_left(self):
+		return self.left != None
+
+	def __has_right(self):
+		return self.right != None
+
+	def __has_both(self):
+		return self.__has_left() and self.__has_right()
+
+	def __has_no_children(self):
+		return (not self.__has_left()) and (not self.__has_right())
+
+	def __is_left_child(self):
+		if self.depth > 0 and self.parent.left != None:
+			return self.parent.left.data == self.data
+		else:
+			return None
+
+	def __is_right_child(self):
+		if self.depth > 0 and self.parent.right != None:
+			return self.parent.right.data == self.data
+		else: 
+			return None
 
 	#add a node to left branch
 	def add(self, data, branch):
 		setattr(self, branch, Node(data))
 		getattr(self, branch).parent = self
+		getattr(self, branch).depth = self.depth + 1
+		getattr(self, branch).branch = branch
 
 	#remove branch, if none of the branch is specified,
 	#remove both
@@ -21,30 +49,49 @@ class Node:
 			setattr(self, node, None)
 
 	#traverse the tree in inorderNone
-	def inorder(self, result = []):
+	def inorder(self, result = [], reset = True):
+		if reset:
+			del result[:]
 		if self.left != None:
-			self.left.inorder(result)
+			self.left.inorder(result, False)
 		result.append(self.data)
 		if self.right != None:
-			self.right.inorder(result)
+			self.right.inorder(result, False)
 		return result
 
 	#traverse the tree in preorder
-	def preorder(self, result = []):
+	def preorder(self, result = [], reset = True):
+		if reset:
+			del result[:]
 		result.append(self.data)
 		if self.left != None:
-			self.left.preorder(result)
+			self.left.preorder(result, False)
 		if self.right != None:
-			self.right.preorder(result)
+			self.right.preorder(result, False)
 		return result
 
 	#traverse the tree in postorder
-	def postorder(self, result = []):
+	def postorder(self, result = [], reset = True):
+		if reset:
+			del result[:]
 		if self.left != None:
-			self.left.postorder(result)
+			self.left.postorder(result, False)
 		if self.right != None:
-			self.right.postorder(result)
+			self.right.postorder(result, False)
 		result.append(self.data)
+		return result
+
+	def get_level(self, level, result = [], reset = True):
+		if reset:
+			del result[:]
+
+		if self.depth < level:
+			if self.__has_left():
+				self.left.get_level(level, result, False)
+			if self.__has_right():
+				self.right.get_level(level, result, False)
+		else:
+			result.append(self.data)
 		return result
 
 	#this function checks whether a data element
@@ -53,17 +100,23 @@ class Node:
 		return (data in self.preorder())
 
 	#this function recursively searches for an element
-	#and returns a the path, by creating the array of
+	#and returns sthe path, by creating the array of
 	#its parents and inversing the array
 	def path(self, data):
 		if (data == self.data):
-			return self
-		if self.left != None:
-			return self.left.path(data)
-		if self.right != None:
-			return self.right.path(data)
+			result = []
+			while (self != None):
+				result.append(self.data)
+				self = self.parent
+			result.reverse()
+			return result
+		else:
+			if self.left != None:
+				return self.left.path(data)
+			if self.right != None:
+				return self.right.path(data) 
 
-	def getNode():
+	def get_node():
 		if (data == self.data):
 			result = []
 			while (self != None):
@@ -84,11 +137,17 @@ class Node_BST(Node):
 		if data > self.data:
 			if (self.right == None):
 				self.right = Node_BST(data)
+				self.right.depth = self.depth + 1
+				self.right.parent = self
+				self.right.branch = "right"
 			else:
 				self.right.add(data)
 		if data < self.data:
 			if (self.left == None):
 				self.left = Node_BST(data)
+				self.left.depth = self.depth + 1
+				self.left.parent = self
+				self.left.branch = "left"
 			else:
 				self.left.add(data)
 
@@ -106,3 +165,30 @@ class Node_BST(Node):
 				return None
 		else:
 			return self
+
+	def find_min(self):
+		while self.left != None:
+			self = self.left
+		return self
+
+
+	def find_max(self):
+		while self.right != None:
+			self = self.right
+		return self
+
+	def rm_node(self, data):
+		node = self.search(data)
+
+		if node == None:
+			return None
+
+		if node.__has_no_children():
+			setattr(node.parent, node.branch, None)
+		elif node.__has_both():
+			temp = node.right.find_min()
+			node.data = temp.data
+			node.right.rm_node(temp.data)
+		else:
+			child = node.left if node.__has_left() else node.right
+			setattr(node.parent, node.branch, child)
--- labs/lab 5/main.py
+++ labs/lab 5/main.py
@@ -1,2 +1,11 @@
 from classes import Node, Node_BST
 
+root = Node(1)
+root.add(2, "left")
+root.add(3, "right")
+root.left.add(4, "left")
+root.left.add(5, "right")
+root.right.add(6, "left")
+root.right.add(7, "right")
+
+print(root.get_level(2))
