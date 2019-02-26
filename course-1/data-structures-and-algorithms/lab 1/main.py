132b22a64e65761f86930b5111230178fc9a0eb9
Viorel Rinja <viorelrinja@gmail.com>
2019-02-26 12:48:53 +0200

    implement qsort with Hoare and Lomuto partition schemes

--- course-1/data-structures-and-algorithms/lab 1/main.py
+++ course-1/data-structures-and-algorithms/lab 1/main.py
@@ -2,10 +2,12 @@ from bubble import bubble
 from selection import selection
 from insertion import insertion
 from qsort import qsort
-# twp insert
-#quick, merge, heap 
 
+#quick, merge, heap , radix/counting sort
+
+
+data = [5, 4, 3, 2, 1]
 # print(insertion.binary([1, 0 ,8, 2, 4, 2, 6, 9 ], 'asc'))
-print(qsort.lomuto([4, 0 ,8, 2, 4, 2, 6, 9 ]))
+print(qsort.hoare(data, 0, len(data)))
 
 
