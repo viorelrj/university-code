a7c9ff35f6ba86349f2a3d776ee8e192cd97dae8
Viorel Rinja <viorelrinja@gmail.com>
2019-02-26 21:49:44 +0200

    Implement unidirectional radix sort

--- course-1/data-structures-and-algorithms/lab 1/main.py
+++ course-1/data-structures-and-algorithms/lab 1/main.py
@@ -3,9 +3,10 @@ from selection import selection
 from insertion import insertion
 from qsort import qsort
 from merge import merge
-#quick, heap , radix/counting sort
+from radix import radix
+#heap , radix/counting sort
 
 
-data = [5, 4, 3, 2, 1, 3, 6, 9, 5, 6]
+data = [1, 64, 72, 112, 59, 902, 1105]
 # # print(insertion.binary([1, 0 ,8, 2, 4, 2, 6, 9 ], 'asc'))
-print(merge.classical(data))
+print(radix(data))
