d319241b4b338be52e963e98568d3731ae409fc2
Viorel Rinja <viorelrinja@gmail.com>
2019-02-26 18:18:55 +0200

    add basic merge sort, only in increasing order

--- course-1/data-structures-and-algorithms/lab 1/main.py
+++ course-1/data-structures-and-algorithms/lab 1/main.py
@@ -2,12 +2,10 @@ from bubble import bubble
 from selection import selection
 from insertion import insertion
 from qsort import qsort
-
-#quick, merge, heap , radix/counting sort
-
-
-data = [5, 4, 3, 2, 1]
-# print(insertion.binary([1, 0 ,8, 2, 4, 2, 6, 9 ], 'asc'))
-print(qsort.hoare(data, 0, len(data)))
+from merge import merge
+#quick, heap , radix/counting sort
 
 
+data = [5, 4, 3, 2, 1, 3, 6, 9, 5, 6]
+# # print(insertion.binary([1, 0 ,8, 2, 4, 2, 6, 9 ], 'asc'))
+print(merge.classical(data))
---
+++ course-1/data-structures-and-algorithms/lab 1/merge.py
@@ -0,0 +1,23 @@
+class Merge:
+	def classical(self, data, method = 'asc'):
+		if len(data) > 1:
+			mid = int(len(data) / 2)
+			left = self.classical(data[0:mid])
+			right = self.classical(data[mid: len(data)])
+			del data[:]
+			while len(left) > 0 or len(right) > 0:
+				if len(left) == 0:
+					data.append(right.pop(0))
+				elif len(right) == 0:
+					data.append(left.pop(0))
+				else:
+					if left[0] <= right[0]:
+						data.append(left.pop(0))
+					else:
+						data.append(right.pop(0))
+			return data
+		else:
+			return data
+
+
+merge = Merge()
