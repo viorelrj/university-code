d8875b4cafb287da8cfda3ada7b9075c27e4bc4f
Viorel Rinja <viorelrinja@gmail.com>
2019-02-26 23:36:19 +0200

    DSA: lab 1: Implement heap sort

--- course-1/data-structures-and-algorithms/lab 1/main.py
+++ course-1/data-structures-and-algorithms/lab 1/main.py
@@ -4,9 +4,10 @@ from insertion import insertion
 from qsort import qsort
 from merge import merge
 from radix import radix
-#heap , radix/counting sort
+from heap import heap
+#heap
 
 
 data = [1, 64, 72, 112, 59, 902, 1105]
 # # print(insertion.binary([1, 0 ,8, 2, 4, 2, 6, 9 ], 'asc'))
-print(radix(data))
+print(heap(data))
