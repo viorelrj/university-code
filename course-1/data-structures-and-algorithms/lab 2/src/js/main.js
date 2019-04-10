a3ed70d9991dd63ac02d3d54530498193691b11c
Viorel Rinja <viorelrinja@gmail.com>
2019-04-10 13:12:21 +0300

    DSA lab-2: add a naive Disjkstra's algorithm modification for finding all minimum distances from one point

--- course-1/data-structures-and-algorithms/lab 2/src/js/main.js
+++ course-1/data-structures-and-algorithms/lab 2/src/js/main.js
@@ -1,20 +1,17 @@
 const canvasX = 640
 const canvasY = 640
 const ELLIPSE_SIZE = 30
-const ARROW_LENGTH = 50
-const POINT = {
-	speed: 8,
-	size: 6,
-}
 
-let vertices = {}
-let adjency = {}
-let distances = {}
+const vertices = {}
+const adjency = {}
+const distances = {}
 
 let edgeStart
 let draggedVertex
 let vertexCounter
 let hasDragged = false
+let vertexInstances = 0
+
 
 // Check if an instance of object has no keys
 function isEmpty(obj) {
@@ -39,8 +36,7 @@ function getHoverLine() {
 	if (!isEmpty(adjency)) {
 		for (let startIndex in adjency) {
 			const start = vertices[startIndex]
-			for (let endVertex of adjency[startIndex]) {
-				const endIndex = endVertex.index
+			for (const endIndex in adjency[startIndex]) {
 				const end = vertices[endIndex]
 				if (collidePointLine(mouseX, mouseY, start.x, start.y, end.x, end.y, 0.5)) {
 					return {startIndex: startIndex, endIndex: endIndex}
@@ -53,68 +49,45 @@ function getHoverLine() {
 
 function setup () {
 	const canvas = createCanvas(canvasX, canvasY);
+	canvas.mouseReleased(handleMouseReleased)
+	canvas.mousePressed(handleMousePressed)
 	vertexCounter = 0
 }
 
-function drawConnections() {
+
+function drawEdges() {
 	//If there is nothing to draw, cancel
 	if (isEmpty(adjency)) {
 		return 
 	}
 
-	for ( let startIndex in adjency ) {
-		const start = vertices[startIndex]
-		for (let connection of adjency[startIndex]) {
-			const end = vertices[connection.index]
+	for (const startVertex in adjency) {
+		for (const endVertex in adjency) {
+			if (adjency[startVertex][endVertex] !== null && endVertex < startVertex) {
+				const start = vertices[startVertex]
+				const end = vertices[endVertex]
+				line(start.x, start.y, end.x, end.y)
 
-			const dx = end.x - start.x
-			const dy = end.y - start.y
-			const slope = dy / dx
-			const distance = Math.floor(Math.sqrt(pow(dx, 2) + pow(dy, 2)))
+				// Get what to write as the length of the edge
+				const length = adjency[startVertex][endVertex]
 
-			//Draw the connection line
-			fill(0)
-			line(start.x, start.y, end.x, end.y)
+				// Get the modification of cansaf required to draw the data correctly
+				const xCenter = (start.x + end.x) / 2
+				const yCenter = (start.y + end.y) / 2
+				const angle = Math.atan((end.y - start.y) / (end.x - start.x)) 
 
-			//Draw the length of line
-			textAlign(CENTER, CENTER)
-			textSize(20);
-			fill(0)
-			translate(start.x + dx / 2, start.y + dy / 2)
-			rotate(Math.atan(slope))
-			text(distance, 20, 20)
-			rotate(-Math.atan(slope))
-			translate(-(start.x + dx / 2), -(start.y + dy / 2))
-		}
-	}
-}
 
-// Conection poins are defined as the points that run across
-// the edges and indicate the direction of the flow
-function drawConnectionPoints() {
-	// If there is nothing to draw, return
-	if (isEmpty(adjency)) {
-		return
-	}
-
-	for (let startIndex in adjency) {
-		const start = vertices[startIndex]
-		for (let connection of adjency[startIndex]) {
-			const end = vertices[connection.index]
-			const dx = end.x - start.x
-			const dy = end.y - start.y
-
-			connection.point.x += dx / Math.hypot(end.x - start.x, end.y, start.y) * POINT.speed
-			connection.point.y += dy / Math.hypot(end.x - start.x, end.y, start.y) * POINT.speed
-
-			if (end.x > start.x && connection.point.x > end.x || end.x < start.x && connection.point.x < end.x) {
-				connection.point.x = start.x
-				connection.point.y = start.y
+				// Draw the text
+				// Move origin point of the canvas to the text position
+				translate(xCenter, yCenter)
+				// Rotate the canvas by the set angle
+				rotate(angle)
+				textSize(24)
+				text(length, 0, 0)
+				// Get the canvas to the original state
+				rotate(-angle)
+				translate(-xCenter, -yCenter)
 			}
-
-
-			fill(255, 116, 116)
-			ellipse(connection.point.x, connection.point.y, POINT.size)
 		}
 	}
 }
@@ -139,13 +112,56 @@ function drawVertices () {
 
 function draw () {
 	clear()
-	drawConnections()
-	drawConnectionPoints()
+	drawEdges()
 	drawVertices()
 }
 
+function addVertex() {
+	vertices[vertexCounter] = { x: mouseX, y: mouseY, isStartpoint: false }
+	adjency[vertexCounter] = {}
+	for (const key in adjency) {
+		adjency[vertexCounter][key] = null
+		adjency[key][vertexCounter] = null
+	}
+	// addjency[vertexCounter]
+	if (edgeStart !== null && edgeStart !== undefined) {
+		console.log(edgeStart)
+		vertices[edgeStart].isStartpoint = false
+	}
+	edgeStart = null;
+	vertexCounter++
+	vertexInstances++
+}
 
-function mouseReleased () {
+function removeVertex(hover) {
+
+	delete vertices[hover]
+	delete adjency[hover]
+	for (const key in adjency) {
+		delete adjency[key][hover]
+	}
+
+	vertexInstances--
+}
+
+function addEdge(hover) {
+	const length = int(window.prompt('What is the length of the edge?'))
+	adjency[edgeStart][hover] = length
+	adjency[hover][edgeStart] = length
+
+	if (edgeStart !== null) {
+		vertices[edgeStart].isStartpoint = false
+	}
+	
+	edgeStart = null
+}
+
+function removeEdge(hoverLine) {
+	adjency[hoverLine.startIndex][hoverLine.endIndex] = null
+	adjency[hoverLine.endIndex][hoverLine.startIndex] = null
+}
+
+function handleMouseReleased () {
 	const hover = getHover()
 	const hoverLine = getHoverLine()
 
@@ -161,50 +177,27 @@ function mouseReleased () {
 		}
 		// If clicked something and clicked another vertex before
 		else if (hover !== null && edgeStart !== null && hover !== edgeStart) {
-			// Add a line to adjency Data structure
-			if (adjency[edgeStart]) {
-				if (!adjency[edgeStart].includes(hover)) {
-					adjency[edgeStart].push({ index: int(hover), point: {x: vertices[edgeStart].x, y: vertices[edgeStart].y} })
-				}
-			} else {
-
-				adjency[edgeStart] = [{ index: int(hover), point: { x: vertices[edgeStart].x, y: vertices[edgeStart].y } }]
-			}
-			// Reset memory of previously clicked vertex
-			vertices[edgeStart].isStartpoint = false
-			edgeStart = null
+			addEdge(hover)
 		} 
 		else {
-			//Add vertex
-			vertices[vertexCounter] = { x: mouseX, y: mouseY, isStartpoint: false}
-			distances[vertexCounter++] = Infinity
-			edgeStart = null;
+			addVertex()
 		}
 	}
 	// If right click
 	else if (mouseButton === 'right') {
 		// If clicked a vertex, remove it
-		if (edgeStart !== null) {
-			vertices[edgeStart].isStartpoint = false
-			edgeStart = null
-		}
 		if (hover !== null) {
-			delete adjency[hover]
-			delete distances[hover]
-			for (let vertex in adjency) {
-				adjency[vertex] = adjency[vertex].filter((item) => item.index !== hover)
-			}
-			delete vertices[hover]
+			removeVertex(hover)
 		}
-		// If clicked a line, remove it
+
 		if (hoverLine !== null) {
-			adjency[hoverLine.startIndex] = adjency[hoverLine.startIndex].filter((item) => item !== hoverLine.endIndex)
+			removeEdge(hoverLine)
 		}
 	}
 	draggedVertex = null
 }
 
-function mousePressed () {
+function handleMousePressed () {
 	const hover = getHover()
 	draggedVertex = hover
 }
@@ -225,6 +218,10 @@ function keyPressed () {
 		console.log({ vertices: vertices, adjency: adjency, distances: distances})
 	}
 
+	if (key === 'e') {
+		drawEdges()
+	}
+
 	// Reset vertex focus
 	if (key === 'r') {
 		if (edgeStart !== null) {
@@ -238,45 +235,77 @@ function keyPressed () {
 
 	// If pressed spacebar
 	if (keyCode === 32) {
-		function hasUnvisited () {
-			for ( const vertex in distances ) {
-				if (distances[vertex] === Infinity) {
-					return true
-				}
-			}
-			return false
-		}
+		buildDistancesTable()
+		findMinDistance()
+	}
+}
 
-		function checkVertex(prev, curr) {
-			if (!hasUnvisited()) {
-				return
-			}
+function buildDistancesTable() {
+	// This function Calculates the shortest distances from one point
+	function minDistances(start) {
+		// This function recursively minimizes the distances to each node
+		// Input: prev - index of the previous vertex, curr - index of the current vertex
+		function getDistance(prev, curr) {
+			visited[curr]++
+
 			if (prev === null) {
-				distances[curr] = 0
-
-				for (next of adjency[curr]) {
-					checkVertex(curr, next.index)
-				}
+				row[curr] = 0
 			} else {
-				const distance = Math.floor(Math.sqrt(Math.pow(vertices[curr].x - vertices[prev].x, 2) + Math.pow(vertices[curr].y - vertices[prev].y, 2)))
-				if (distances[prev] + distance < distances[curr]) {
-					distances[curr] = distances[prev] + distance
+				if (adjency[prev][curr] + row[prev] <= row[curr]) {
+					row[curr] = adjency[prev][curr] + row[prev]
 				}
+			}
 
-				if (adjency[curr]) {
-					for (next of adjency[curr]) {
-						(curr !== next) ? checkVertex(curr, next) : null
+			if (visited !== {}) {
+				for (next in adjency[curr]) {
+					if (adjency[curr][next] !== null && visited[next] < vertexInstances) {
+						getDistance(curr, next)
 					}
 				}
-
 			}
 		}
+		
+		// Select a row to calcuate distances from
+		const row = Object.assign({}, adjency[start]);
+		const visited = Object.assign({}, adjency[start]);
 
-		for (vertex in distances) {
-			distances[vertex] = Infinity
+		// Build initial distances with infinity as the initial value
+		for (const key in row) {
+			row[key] = Infinity
+			visited[key] = 0
 		}
+		row[start] = 0	
 
-		checkVertex(null, hover)
+		// Recursively minimize the distances
+		getDistance(null, start)
+		// Save the row into the distances table
+		distances[start] = Object.assign({}, row);
 	}
-	
+
+
+	// Build row of distances for every vertex
+	for (key in vertices) {
+		minDistances(int(key))
+	}
+}
+
+function findMinDistance() {
+	const rows = {}
+	for (const rowIndex in distances) {
+		for (const element in distances[rowIndex]) {
+			if (rows[rowIndex] === undefined) {
+				rows[rowIndex] = 0
+			}
+			rows[rowIndex] += distances[rowIndex][element]
+		}
+	}
+	let minRow = Infinity
+	let minRowIndex
+	for (const key in rows) {
+		if (rows[key] < minRow) {
+			minRow = rows[key]
+			minRowIndex = int(key)
+		}
+	}
+	console.log(minRow, minRowIndex)
 }
