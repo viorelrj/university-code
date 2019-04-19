b1de75ff33abad2b1353cbb948eaf6d7a5b3e33a
Viorel Rinja <viorelrinja@gmail.com>
2019-04-19 03:06:19 +0300

    DSA lab2: stable graph algorithms

--- course-1/data-structures-and-algorithms/lab 2/src/js/main.js
+++ course-1/data-structures-and-algorithms/lab 2/src/js/main.js
@@ -1,10 +1,11 @@
-const canvasX = 640
-const canvasY = 640
+const canvasX = 1280
+const canvasY = 720
 const ELLIPSE_SIZE = 30
 
 const vertices = {}
 const adjency = {}
-const distances = {}
+
+let distances = {}
 
 let edgeStart
 let draggedVertex
@@ -12,6 +13,19 @@ let vertexCounter
 let hasDragged = false
 let vertexInstances = 0
 
+// MinCum - Minimum cummulative (median)
+let minCumRow = Infinity
+let minCumRowIndex
+
+// (Center)
+let minRow = Infinity
+let minRowIndex
+
+let pCenters = []
+let pMedians = []
+
+
+// Functionality
 
 // Check if an instance of object has no keys
 function isEmpty(obj) {
@@ -47,6 +61,10 @@ function getHoverLine() {
 	return null
 }
 
+
+
+// Drawing library specific functions
+
 function setup () {
 	const canvas = createCanvas(canvasX, canvasY);
 	canvas.mouseReleased(handleMouseReleased)
@@ -54,6 +72,14 @@ function setup () {
 	vertexCounter = 0
 }
 
+function draw() {
+	clear()
+	drawEdges()
+	drawVertices()
+}
+
+
+// GUI drawing specific functions
 
 function drawEdges() {
 	//If there is nothing to draw, cancel
@@ -95,14 +121,57 @@ function drawEdges() {
 function drawVertices () {
 	textAlign(CENTER, CENTER)
 	textSize(11);
+
+	// Green - selected start point
+	// Blue - is a graph median - has shortest routes to every edge
+	// Red - is a graph center - has the minimum of greatest distance
+	// Pink - both center and median
+
 	for (let i in vertices) {
-		if (vertices[i].isStartpoint) {
-			fill(0, 255, 0)
+
+		fill(255)
+		ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
+		fill(0)
+		text(i, vertices[i].x, vertices[i].y)
+
+		//If is a pMedian
+		if (pMedians.includes(int(i))) {
+			fill(0, 80, 255)
+			ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
+			fill(255)
+			text(i, vertices[i].x, vertices[i].y)
+			fill(0)
+		}
+		//If is a pCenter
+		if (pCenters.includes(int(i))) {
+			fill(255, 80, 0)
 			ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
 			fill(0)
 			text(i, vertices[i].x, vertices[i].y)
-		} else {
+		}
+		//If is both pMedian and pCenter
+		if (pMedians.includes(int(i)) && pCenters.includes(int(i))) {
+			fill(255, 80, 255)
+			ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
+			fill(0)
+			text(i, vertices[i].x, vertices[i].y)
+		}
+		//is a Center
+		if (minRowIndex === int(i)){
+			fill(255, 0, 0)
+			ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
+			fill(0)
+			text(i, vertices[i].x, vertices[i].y)
+		}
+		//is a Median
+		if (minCumRowIndex === int(i)) {
+			fill(0, 0, 255)
+			ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
 			fill(255)
+			text(i, vertices[i].x, vertices[i].y)
+		}
+		if (vertices[i].isStartpoint) {
+			fill(0, 255, 0)
 			ellipse(vertices[i].x, vertices[i].y, ELLIPSE_SIZE)
 			fill(0)
 			text(i, vertices[i].x, vertices[i].y)
@@ -110,11 +179,7 @@ function drawVertices () {
 	}
 }
 
-function draw () {
-	clear()
-	drawEdges()
-	drawVertices()
-}
+// GUI logic
 
 function addVertex() {
 	vertices[vertexCounter] = { x: mouseX, y: mouseY, isStartpoint: false }
@@ -125,7 +190,6 @@ function addVertex() {
 	}
 	// addjency[vertexCounter]
 	if (edgeStart !== null && edgeStart !== undefined) {
-		console.log(edgeStart)
 		vertices[edgeStart].isStartpoint = false
 	}
 	edgeStart = null;
@@ -157,8 +221,10 @@ function addEdge(hover) {
 }
 
 function removeEdge(hoverLine) {
-	adjency[hoverLine.startIndex][hoverLine.endIndex] = null
-	adjency[hoverLine.endIndex][hoverLine.startIndex] = null
+	if (hoverLine !== null) {
+		adjency[hoverLine.startIndex][hoverLine.endIndex] = null
+		adjency[hoverLine.endIndex][hoverLine.startIndex] = null
+	}
 }
 
 function handleMouseReleased () {
@@ -218,10 +284,6 @@ function keyPressed () {
 		console.log({ vertices: vertices, adjency: adjency, distances: distances})
 	}
 
-	if (key === 'e') {
-		drawEdges()
-	}
-
 	// Reset vertex focus
 	if (key === 'r') {
 		if (edgeStart !== null) {
@@ -233,79 +295,171 @@ function keyPressed () {
 		}
 	}
 
+	// find P centers
+	if (key === 'c') {
+		const p = int(window.prompt('How many centers?'))
+		pCenter(p)
+	}
+
+	// find P medians
+	if (key === 'm') {
+		const p = int(window.prompt('How many medians?'))
+		pMedian(p)
+	}
+
 	// If pressed spacebar
 	if (keyCode === 32) {
-		buildDistancesTable()
-		findMinDistance()
+		getCenterAndMedian()
 	}
 }
 
-function buildDistancesTable() {
-	// This function Calculates the shortest distances from one point
-	function minDistances(start) {
-		// This function recursively minimizes the distances to each node
-		// Input: prev - index of the previous vertex, curr - index of the current vertex
-		function getDistance(prev, curr) {
-			visited[curr]++
 
-			if (prev === null) {
-				row[curr] = 0
+
+// Graph logic
+function warshall() {
+	distances = JSON.parse(JSON.stringify(adjency))
+
+	for (let i in distances) {
+		for (let k in distances) {
+			if (i == k) {
+				distances[i][k] = 0
 			} else {
-				if (adjency[prev][curr] + row[prev] <= row[curr]) {
-					row[curr] = adjency[prev][curr] + row[prev]
-				}
-			}
-
-			if (visited !== {}) {
-				for (next in adjency[curr]) {
-					if (adjency[curr][next] !== null && visited[next] < vertexInstances) {
-						getDistance(curr, next)
-					}
-				}
+				distances[i][k] = (distances[i][k] == null) ? Infinity : distances[i][k]
 			}
 		}
-		
-		// Select a row to calcuate distances from
-		const row = Object.assign({}, adjency[start]);
-		const visited = Object.assign({}, adjency[start]);
-
-		// Build initial distances with infinity as the initial value
-		for (const key in row) {
-			row[key] = Infinity
-			visited[key] = 0
-		}
-		row[start] = 0	
-
-		// Recursively minimize the distances
-		getDistance(null, start)
-		// Save the row into the distances table
-		distances[start] = Object.assign({}, row);
 	}
 
-
-	// Build row of distances for every vertex
-	for (key in vertices) {
-		minDistances(int(key))
+	for (let k in distances) {
+		for (let i in distances) {
+			for (let j in distances) {
+				if (distances[i][k] + distances[k][j] < distances[i][j]) {
+					distances[i][j] = distances[i][k] + distances[k][j]
+				}
+			}
+		}
 	}
 }
 
-function findMinDistance() {
+function getCenterAndMedian() {
+	minCumRow = Infinity
+	minCumRowIndex = null
+	minRow = Infinity
+	minRowIndex
+	
+	warshall()
+
 	const rows = {}
+	const cols = {}
+
+	const maxRows = {}
+
+	// Building the rows and cols lengths
 	for (const rowIndex in distances) {
 		for (const element in distances[rowIndex]) {
-			if (rows[rowIndex] === undefined) {
-				rows[rowIndex] = 0
-			}
+			if (rows[rowIndex] === undefined) rows[rowIndex] = 0
+			if (cols[element] === undefined) cols[element] = 0
 			rows[rowIndex] += distances[rowIndex][element]
+			cols[element] += distances[element][rowIndex]
+
+			if (distances[rowIndex][element] > maxRows[rowIndex] || maxRows[rowIndex] === undefined) {
+				maxRows[rowIndex] = distances[rowIndex][element]
+			}
 		}
 	}
-	let minRow = Infinity
-	let minRowIndex
+
+
+	// Searching for minimum
 	for (const key in rows) {
-		if (rows[key] < minRow) {
-			minRow = rows[key]
+		if (rows[key] < minCumRow) {
+			minCumRow = rows[key]
+			minCumRowIndex = int(key)
+		}
+
+		if (maxRows[key] < minRow) {
+			minRow = maxRows[key]
 			minRowIndex = int(key)
 		}
+		// if (cols[key] < minCol) {
+		// 	minCol = cols[key]
+		// 	minColIndex = int(key)
+		// }
 	}
-	console.log(minRow, minRowIndex)
+}
+
+function pCenter(p) {
+	pCenters = []
+	warshall()
+
+	// solution should be an array
+	function checkSolution(solution) {
+		let minDistances = []
+		for(let colIndex in distances) {
+			let col = []
+			if (!solution.includes(colIndex)) {
+				col = solution.map(sol => distances[sol][colIndex])
+			}
+			minDistances.push(Math.min(...col))
+		}
+
+		return Math.max(...minDistances)
+	}
+
+
+	// Get initial set of solutions
+	let unchecked = Object.keys(vertices).map(x => int(x))
+
+	// Populate initial solution
+	while(pCenters.length < p) {
+		pCenters.push(unchecked.pop())
+	}
+
+	// Try the element in solution set
+	for (let variant of unchecked) {
+		for (let index in pCenters) {
+			let possibleSolution = JSON.parse(JSON.stringify(pCenters))
+			possibleSolution[index] = variant
+			if (checkSolution(possibleSolution) < checkSolution(pCenters)) {
+				pCenters = possibleSolution
+			}
+		}
+	}
+}
+
+function pMedian(p) {
+	pMedians = []
+
+	warshall()
+	// solution should be an array
+	function checkSolution(solution) {
+		let cumulatives = []
+		for (let sol of solution) {
+			let currSum = 0
+			for (let col in distances) {
+				currSum += distances[sol][col]
+			}
+			cumulatives.push(currSum)
+		}
+
+		return Math.min(...cumulatives)
+	}
+
+
+	// Get initial set of solutions
+	let unchecked = Object.keys(vertices).map(x => int(x))
+
+	// Populate initial solution
+	while (pMedians.length < p) {
+		pMedians.push(unchecked.pop())
+	}
+
+	// Try the element in solution set
+	for (let variant of unchecked) {
+		for (let index in pMedians) {
+			let possibleSolution = JSON.parse(JSON.stringify(pMedians))
+			possibleSolution[index] = variant
+			if (checkSolution(possibleSolution) < checkSolution(pMedians)) {
+				pMedians = possibleSolution
+			}
+		}
+	}
 }
